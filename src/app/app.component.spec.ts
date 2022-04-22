5dabecc7c264fbf0837a097a19ea4629ccc982d7
root <root@iPhone>
2022-04-22 04:41:38 -0400

    login

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shreyu'`, () => {
+  it(`should have as title 'Simplicity'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('shreyu');
+    expect(app.title).toEqual('Simplicity');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to shreyu!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Simplicity!');
   });
 });
